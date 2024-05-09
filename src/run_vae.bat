e065c2a7c9ec7d56e2abf61383f5003c7afc9a99
LohithSai Yadala Chanchu <L.S.YadalaChanchu@student.tudelft.nl>
2024-05-09 23:55:19 +0200

    Add files

--- src/run_vae.bat
+++ src/run_vae.bat
@@ -1,6 +1,6 @@
-python federated_main.py --model=vae --dataset=mnist --gpu=cuda:0 --iid=2 --epochs=30 --dirichlet=0.2 --frac=1.0 --num_users=10 --local_ep=10
-python federated_main.py --model=vae --dataset=mnist --gpu=cuda:0 --iid=2 --epochs=30 --dirichlet=0.4 --frac=1.0 --num_users=10 --local_ep=10
-python federated_main.py --model=vae --dataset=mnist --gpu=cuda:0 --iid=2 --epochs=30 --dirichlet=0.6 --frac=1.0 --num_users=10 --local_ep=10
-python federated_main.py --model=vae --dataset=mnist --gpu=cuda:0 --iid=2 --epochs=30 --dirichlet=0.8 --frac=1.0 --num_users=10 --local_ep=10
-python federated_main.py --model=vae --dataset=mnist --gpu=cuda:0 --iid=2 --epochs=30 --dirichlet=1.0 --frac=1.0 --num_users=10 --local_ep=10
+python federated_main.py --model=vae --dataset=mnist --gpu=cuda:0 --iid=2 --epochs=30 --dirichlet=0.1 --frac=1.0 --num_users=10 --local_ep=10
+python federated_main.py --model=vae --dataset=mnist --gpu=cuda:0 --iid=2 --epochs=30 --dirichlet=0.3 --frac=1.0 --num_users=10 --local_ep=10
+python federated_main.py --model=vae --dataset=mnist --gpu=cuda:0 --iid=2 --epochs=30 --dirichlet=0.5 --frac=1.0 --num_users=10 --local_ep=10
+python federated_main.py --model=vae --dataset=mnist --gpu=cuda:0 --iid=2 --epochs=30 --dirichlet=0.7 --frac=1.0 --num_users=10 --local_ep=10
+python federated_main.py --model=vae --dataset=mnist --gpu=cuda:0 --iid=2 --epochs=30 --dirichlet=0.9 --frac=1.0 --num_users=10 --local_ep=10
 
